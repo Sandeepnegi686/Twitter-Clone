6c59e8448ba3ff2f2c8c34193c08ecb4deba76a3
Sandeep Negi <39149906+Sandeepnegi686@users.noreply.github.com>
2026-02-26 00:54:50 +0530

    updated backend code for getuserbyId

--- server/src/Routes/auth.ts
+++ server/src/Routes/auth.ts
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 import { loginUser, signUp } from "../Controller/UserController";
 import authenticateUser from "../middleware/authMiddleware";
 import UserModel from "../Model/UserModel";
+import { Types } from "mongoose";
 
 const router = express.Router();
 const CLIENT_URL = process.env.CLIENT_URL || "";
@@ -18,6 +19,15 @@ router.get("/me", authenticateUser, (req: Request, res: Response) => {
   return res.status(200).json({ success: true, user: req.user });
 });
 
+router.get("/user/:id", async (req: Request<{ id: string }>, res: Response) => {
+  if (!req.params.id || !Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ success: false, message: "id is invalid" });
+  }
+  const user = await UserModel.findById(req.params.id);
+  if (!user)
+    return res.status(400).json({ success: false, message: "user not found" });
+  return res.status(200).json({ success: true, user });
+});
 router.get("/users", async (req: Request, res: Response) => {
   const users = await UserModel.find().sort({ createdAt: -1 });
   return res.status(200).json({ success: true, users });
