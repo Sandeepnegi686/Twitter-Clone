4c12850d94dc5313356b08fa91aa481eb3d62be4
Sandeep Negi <39149906+Sandeepnegi686@users.noreply.github.com>
2026-03-07 00:16:34 +0530

    working on notifications

--- client/app/notifications/page.tsx
+++ client/app/notifications/page.tsx
@@ -2,7 +2,10 @@ import Header from "../_components/Header";
 import NotificationsFeed from "../_components/NotificationsFeed";
 
 export default async function Page() {
-  const notifications = await fetch("/api/notification", { method: "GET" });
+  const url = process.env.DOMAIN_URL || "";
+  const notifications = await fetch(`${url}/api/notification`, {
+    credentials: "include",
+  });
   console.log(notifications);
 
   return (
