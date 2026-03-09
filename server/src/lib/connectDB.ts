36f6bffd9e05aa90b7cdbbaaab843ec019dc3f95
Sandeep <sandeepnegi686@gmail.com>
2026-03-09 15:56:32 +0530

    updated db connection for serverless

--- server/src/lib/connectDB.ts
+++ server/src/lib/connectDB.ts
@@ -1,61 +1,10 @@
-// import mongoose from "mongoose";
-
-// function connectDB(url: string) {
-//   return mongoose.connect(url);
-// }
-// export default connectDB;
 import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI as string;
+let cached = (global as any).mongoose as any;
 
-// if (!MONGODB_URI) {
-//   throw new Error(
-//     "Please define the MONGODB_URI environment variable inside .env.local",
-//   );
-// }
+export async function connectDB(db_url: string) {
+  if (cached) return cached;
 
-/** * Global is used here to maintain a cached connection across hot reloads
- * in development and function executions in serverless environments.
- */
-interface MongooseCache {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
+  cached = await mongoose.connect(db_url);
+  return cached;
 }
-
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: MongooseCache | undefined;
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect(MONGODB_URIL: string) {
-  if (cached?.conn) {
-    return cached.conn;
-  }
-
-  if (!cached?.promise) {
-    const opts = {
-      bufferCommands: false, // Disable buffering to catch connection errors early
-    };
-
-    cached!.promise = mongoose.connect(MONGODB_URIL, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  try {
-    cached!.conn = await cached!.promise;
-  } catch (e) {
-    cached!.promise = null;
-    throw e;
-  }
-
-  return cached!.conn;
-}
-
-export default dbConnect;
